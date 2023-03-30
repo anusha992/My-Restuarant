58b1ecd5898c95c46ac2d762e113c6c54b17d421
anusha992 <129392505+anusha992@users.noreply.github.com>
2023-03-30 18:22:50 +0530

    Add files via upload
    Signed-off-by: anusha992 <129392505+anusha992@users.noreply.github.com>

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from  'react'
+
+const Home = () => {
+    return (
+        <div>
+            <center>
+    <h4>Home component</h4>            
+            </center>
+        </div>
+    )
+}
+export default Home
